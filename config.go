363957a3d4181a482b80c07c0b051f1feba9dd50
Ronald G. Minnich <rminnich@gmail.com>
2019-02-12 17:43:10 +0000

    Add copyrights where missing
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

--- config.go
+++ config.go
@@ -1,3 +1,6 @@
+// Copyright 2012-2018 the u-root Authors. All rights reserved
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
 
 package main
 
@@ -2944,4 +2947,3 @@ CONFIG_NO_STAGE_CACHE=y
 CONFIG_C_ENVIRONMENT_BOOTBLOCK=y
 `
 )
-
